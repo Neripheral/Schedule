44f1b7dbf5582166173ca05cecbf46b24bdadacc
Neripheral <neripheral@gmail.com>
2023-03-14 17:41:36 +0100

    test: declare ScheduleTest and implement singleStep

---
+++ src/test/java/ScheduleTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import schedule.Schedule;
+import schedule.Schedules;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class ScheduleTest {
+    private Schedule s;
+    private Model model;
+
+    @BeforeEach
+    void setUp() {
+        model = new Model();
+    }
+
+    @Test
+    public void singleStepScheduleExecutesCorrectly() {
+        assertThat(model.age).isNotEqualTo(5);
+        s = Schedules.perform(()-> model.age = 5);
+        assertThat(s.proceed()).isTrue();
+        assertThat(model.age).isEqualTo(5);
+        assertThat(s.proceed()).isFalse();
+    }
+
+
+
+    private static class Model{
+        public int age = 0;
+        public String name = "John";
+        public String surname = "Smith";
+    }
+}
