762e07bc3b8d04eb152667fbff2c6f6c88d5a084
Neripheral <neripheral@gmail.com>
2023-03-17 20:04:16 +0100

    feat: implement Schedules.list

--- src/main/java/schedule/Schedules.java
+++ src/main/java/schedule/Schedules.java
@@ -1,9 +1,18 @@
 package schedule;
 
+import java.util.Arrays;
+
 public enum Schedules {;
     public static Schedule step(Runnable procedure){
         if(procedure == null)
             return EmptyStep.INSTANCE;
         return Step.of(procedure);
+    }
+
+    public static Schedule list(Schedule...schedules){
+        if(schedules.length == 0)
+            return EmptyStep.INSTANCE;
+        return Duo.of(schedules[0],
+                list(Arrays.copyOfRange(schedules, 1, schedules.length)));
     }
 }
