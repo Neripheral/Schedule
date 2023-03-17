752d2edd0cee30425e165f08ff51fc27137a45bc
Neripheral <neripheral@gmail.com>
2023-03-17 19:14:48 +0100

    test: extract ScheduleTest.Model

---
+++ src/test/java/ScheduleModel.java
@@ -0,0 +1,5 @@
+class ScheduleModel {
+    public int age = 0;
+    public String name = "John";
+    public String surname = "Smith";
+}
--- src/test/java/ScheduleTest.java
+++ src/test/java/ScheduleTest.java
@@ -7,12 +7,12 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class ScheduleTest {
     private Schedule s;
-    private Model model;
+    private ScheduleModel model;
 
     @BeforeEach
     void setUp() {
         s = Schedules.step(()-> model.age = 5);
-        model = new Model();
+        model = new ScheduleModel();
     }
 
     @Test
@@ -39,9 +39,4 @@ public class ScheduleTest {
         assertThat(model.age).isEqualTo(5);
     }
 
-    private static class Model{
-        public int age = 0;
-        public String name = "John";
-        public String surname = "Smith";
-    }
 }
