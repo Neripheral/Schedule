833bf21145f046f9851ae881116f58ad110113e6
Neripheral <neripheral@gmail.com>
2023-03-18 18:47:18 +0100

    feat: implement Fork

---
+++ src/main/java/schedule/Fork.java
@@ -0,0 +1,37 @@
+package schedule;
+
+import java.util.Objects;
+import java.util.function.BooleanSupplier;
+
+public class Fork implements Schedule{
+    public Fork(BooleanSupplier condition, Schedule scheduleIfPassed, Schedule scheduleIfFailed){
+        this.condition = Objects.requireNonNull(condition);
+        this.scheduleIfPassed = Objects.requireNonNull(scheduleIfPassed);
+        this.scheduleIfFailed = Objects.requireNonNull(scheduleIfFailed);
+    }
+
+    @Override
+    public boolean proceed() {
+        if(!isConditionChecked) {
+            isRequirementPassed = condition.getAsBoolean();
+            isConditionChecked = true;
+        }
+        if(isRequirementPassed)
+            return scheduleIfPassed.proceed();
+        else
+            return scheduleIfFailed.proceed();
+    }
+
+    @Override
+    public void reset() {
+        scheduleIfPassed.reset();
+        scheduleIfFailed.reset();
+        isConditionChecked = false;
+    }
+
+    private boolean isConditionChecked = false;
+    private boolean isRequirementPassed = false;
+    private final BooleanSupplier condition;
+    private final Schedule scheduleIfPassed;
+    private final Schedule scheduleIfFailed;
+}
