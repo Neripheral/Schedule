9edf66b11211f70bf6a623233e72b6e590d8b571
Neripheral <neripheral@gmail.com>
2023-03-27 19:23:27 +0200

    feat: implement Multiple

---
+++ src/main/java/schedule/Multiple.java
@@ -0,0 +1,31 @@
+package schedule;
+
+public class Multiple implements Schedule{
+    private int repeatsExecuted = 0;
+    private final Schedule scheduleToRepeat;
+
+    public Multiple(int repeatsExpected, Schedule scheduleToRepeat) {
+        this.scheduleToRepeat =
+                new Repetition(
+                        ()->{
+                            boolean shouldContinue = repeatsExecuted < repeatsExpected;
+                            if(shouldContinue){
+                                repeatsExecuted++;
+                                return true;
+                            }
+                            return false;
+                        },
+                        scheduleToRepeat
+                );
+    }
+
+    @Override
+    public boolean proceed() {
+        return scheduleToRepeat.proceed();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
