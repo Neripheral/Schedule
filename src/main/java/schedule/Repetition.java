46f4d983e5b84d13a5902e7b492cb18ea31bb23a
Neripheral <neripheral@gmail.com>
2023-03-27 12:04:38 +0200

    feat: implement Repetition

---
+++ src/main/java/schedule/Repetition.java
@@ -0,0 +1,30 @@
+package schedule;
+
+import java.util.function.BooleanSupplier;
+
+public class Repetition implements Schedule {
+    private boolean isDone = false;
+    private final Schedule schedule;
+    private final BooleanSupplier repeatCondition;
+
+    public Repetition(Schedule schedule, BooleanSupplier repeatCondition) {
+        this.schedule = schedule;
+        this.repeatCondition = repeatCondition;
+    }
+
+    @Override
+    public boolean proceed() {
+        if(isDone)
+            return false;
+        if(repeatCondition.getAsBoolean())
+            return schedule.proceed();
+        isDone = true;
+        return false;
+    }
+
+    @Override
+    public void reset() {
+        isDone = false;
+        schedule.reset();
+    }
+}
