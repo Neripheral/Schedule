554070c1682ed914a32500daee7b4eb6d74bc093
Neripheral <neripheral@gmail.com>
2023-03-27 19:53:56 +0200

    feat: implement Standby

---
+++ src/main/java/schedule/Standby.java
@@ -0,0 +1,25 @@
+package schedule;
+
+import java.util.function.BooleanSupplier;
+
+public class Standby implements Schedule {
+    private final Schedule schedule;
+
+    public Standby(BooleanSupplier waitingFor) {
+        schedule =
+                new Repetition(
+                        ()-> !waitingFor.getAsBoolean(),
+                        new Step(()->{})
+                );
+    }
+
+    @Override
+    public boolean proceed() {
+        return schedule.proceed();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
