9a03ef3414a4fb973c6254cdc07d2713da6fe261
Neripheral <neripheral@gmail.com>
2023-03-27 19:26:38 +0200

    test: implement MultipleResetTest

---
+++ src/test/java/MultipleResetTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class MultipleResetTest extends MultipleTest {
+    @Override
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();
+        while(multiple.proceed());
+        multiple.reset();
+        model = ScheduleModel.getFreshModel();
+    }
+}
