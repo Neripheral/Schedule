26dadab53588eea348f03aa2a49fc476423686d3
Neripheral <neripheral@gmail.com>
2023-03-27 19:56:40 +0200

    test: implement StandbyResetTest

---
+++ src/test/java/StandbyResetTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class StandbyResetTest extends StandbyTest {
+    @Override
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();
+        standby.proceed();
+        model.age = 10;
+        while(standby.proceed());
+        standby.reset();
+        model = ScheduleModel.getFreshModel();
+    }
+}
