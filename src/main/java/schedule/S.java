069cc4035a2b2104eb1a763cbd3c168fa4e29547
Neripheral <neripheral@gmail.com>
2023-03-30 17:13:09 +0200

    feat: implement S

---
+++ src/main/java/schedule/S.java
@@ -0,0 +1,59 @@
+package schedule;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.BooleanSupplier;
+
+public enum S {;
+    public static Schedule perform(Runnable functionToPerform){
+        return new Step(Objects.requireNonNull(functionToPerform));
+    }
+
+    public static Schedule list(Schedule...schedules){
+        if(schedules.length == 0)
+            throw new IllegalArgumentException("Array of arguments expected. None supplied.");
+
+        else if(schedules.length == 1)
+            return schedules[0];
+
+        Schedule remainingScheduleTree = list(Arrays.copyOfRange(schedules, 1, schedules.length));
+        return new Duo(schedules[0], remainingScheduleTree);
+    }
+
+    public static Schedule performIf(BooleanSupplier condition, Schedule scheduleToPerformIfTrue){
+        Objects.requireNonNull(condition);
+        Objects.requireNonNull(scheduleToPerformIfTrue);
+
+        return performIf(condition, scheduleToPerformIfTrue, EmptyStep.INSTANCE);
+    }
+
+    public static Schedule performIf(BooleanSupplier condition, Schedule scheduleToPerformIfTrue, Schedule scheduleToPerformIfFalse){
+        Objects.requireNonNull(condition);
+        Objects.requireNonNull(scheduleToPerformIfTrue);
+        Objects.requireNonNull(scheduleToPerformIfFalse);
+
+        return new Fork(condition, scheduleToPerformIfTrue, scheduleToPerformIfFalse);
+    }
+
+    public static Schedule repeatFor(int i, Schedule scheduleToRepeat){
+        Objects.requireNonNull(scheduleToRepeat);
+        if(i < 0) {
+            throw new IllegalArgumentException("Expected i to be positive, got: " + i);
+        }
+
+        return new Multiple(i, scheduleToRepeat);
+    }
+
+    public static Schedule repeatIfTrue(BooleanSupplier condition, Schedule scheduleToRepeat){
+        Objects.requireNonNull(condition);
+        Objects.requireNonNull(scheduleToRepeat);
+
+        return new Repetition(condition, scheduleToRepeat);
+    }
+
+    public static Schedule waitFor(BooleanSupplier condition){
+        Objects.requireNonNull(condition);
+
+        return new Standby(condition);
+    }
+}
